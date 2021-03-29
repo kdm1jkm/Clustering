f9ff84ce8393066b50e3b4c211bfa9aa20f48c64
kdm1jkm <kdm1jkmm@gmail.com>
2021-03-29 16:49:36 +0900

    웹 사이트 받아오기 & 파싱 작업을 멀티스레드로 진행
    대략 50%정도의 성능 향상

--- src/main/java/com/github/kdm1jkm/clustering/Main.java
+++ src/main/java/com/github/kdm1jkm/clustering/Main.java
@@ -1,12 +1,27 @@
 package com.github.kdm1jkm.clustering;
 
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        for (String keyWord : new String[]{"밥", "나무위키", "에러"}) {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        List<Callable<Map<String, Integer>>> tasks = new ArrayList<>();
+
+        for (String keyWord : new String[]{"밥", "나무위키", "에러", "사과", "죽"}) {
             NamuDoc namuDoc = new NamuDoc(keyWord);
-            System.out.println(namuDoc.analyze());
+            tasks.add(namuDoc::analyze);
         }
+
+        List<Future<Map<String, Integer>>> results = executorService.invokeAll(tasks);
+
+        for (Future<Map<String, Integer>> result : results) {
+            Map<String, Integer> stringIntegerMap = result.get();
+            System.out.println(stringIntegerMap);
+        }
+
+        executorService.shutdown();
     }
 }
